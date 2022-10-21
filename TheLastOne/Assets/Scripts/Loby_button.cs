fb95b0235a7fa85c01703a28423e2f4900a1db3d
Nemesis03 <lowhook03@gmail.com>
2022-10-22 02:16:01 +0900

    init

---
+++ TheLastOne/Assets/Scripts/Loby_button.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loby_button : MonoBehaviour
+{
+    public GameObject Stage_Select;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void start_button()
+    (
+        Stage_Select.SetActive(true);
+    }
+}
