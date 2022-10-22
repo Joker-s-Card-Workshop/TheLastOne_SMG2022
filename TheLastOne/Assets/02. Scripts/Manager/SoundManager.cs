4db76563474ddf4ea81ca432dfaadddf9794d2ed
Black_Penguin <dkstmdwnswns@gmail.com>
2022-10-22 09:20:08 +0900

    SoundManager

--- TheLastOne/Assets/02. Scripts/Manager/SoundManager.cs
+++ TheLastOne/Assets/02. Scripts/Manager/SoundManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SoundManager : MonoBehaviour
+using System;
+public enum SoundType
 {
-    // Start is called before the first frame update
-    void Start()
+    SFX,
+    BGM,
+    END
+}
+public class AudioSourceClass
+{
+    public AudioSource audioSource;
+    public float audioVolume;
+}
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+    public Dictionary<SoundType, AudioSourceClass> audioSourceClasses = new Dictionary<SoundType, AudioSourceClass>();
+    private void Awake()
     {
-        
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Sounds/");
+        foreach (AudioClip clip in clips)
+        {
+            audioClips[clip.name] = clip;
+        }
+
+        string[] enumNames = Enum.GetNames(typeof(SoundType));
+        for (int i = 0; i < (int)SoundType.END; i++)
+        {
+            GameObject AudioSourceObj = new GameObject(enumNames[i]);
+            AudioSourceObj.transform.SetParent(transform);
+            AudioSourceClass sourceClass
+                = new AudioSourceClass { audioSource = AudioSourceObj.AddComponent<AudioSource>(), audioVolume = 0.5f };
+            audioSourceClasses[(SoundType)i] = sourceClass;
+        }
+
+        audioSourceClasses[SoundType.BGM].audioSource.loop = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public AudioClip PlaySoundClip(string clipName, SoundType type, float volume = 0.5f, float pitch = 1)
     {
-        
+        AudioClip clip = audioClips[clipName];
+        audioSourceClasses[type].audioSource.pitch = pitch;
+
+        float curVolume = volume * audioSourceClasses[type].audioVolume;
+        if (type == SoundType.BGM)
+        {
+            audioSourceClasses[SoundType.BGM].audioSource.clip = clip;
+            audioSourceClasses[SoundType.BGM].audioSource.volume = curVolume;
+            audioSourceClasses[SoundType.BGM].audioSource.Play();
+        }
+        else
+        {
+            audioSourceClasses[type].audioSource.PlayOneShot(clip, curVolume);
+        }
+
+        return clip;
     }
+
 }
