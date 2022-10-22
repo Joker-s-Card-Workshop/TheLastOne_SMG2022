a95ba2b7c93d84b824fec12f1bb5e7989a0c3f3f
Nemesis03 <lowhook03@gmail.com>
2022-10-22 20:20:25 +0900

    add : CardMerge

---
+++ TheLastOne/Assets/02. Scripts/CardMerge.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMerge : MonoBehaviour
+{
+    List<CardData> FindCardData(List<string> result)
+    {
+        List<CardData> cardData = CardDataInit.Instance.Data;
+        List<CardData> returndata = new List<CardData>();
+        for (var i = 0; i < result.Count; i++)
+        {
+            for (int k = 0; k < cardData.Count; k++)
+            {
+                if (result[i] == cardData[k].CardName)
+                    returndata.Add(cardData[k]);
+            }
+        }
+        return returndata;
+    }
+    List<CardData> CardMergeGet(CardData cardA, CardData cardB)
+    {
+        List<CardMergeData> MergeData = CardMergeInit.Instance.Data;
+        for (var i = 0; i < MergeData.Count; i++)
+        {
+            if (MergeData[i].SourceCard[0] == cardA.CardName)
+            {
+                if (MergeData[i].SourceCard[1] == cardB.CardName)
+                {
+                    return FindCardData(MergeData[i].ResultCard);
+                }
+            }
+            else if (MergeData[i].SourceCard[0] == cardB.CardName)
+            {
+                if (MergeData[i].SourceCard[1] == cardA.CardName)
+                {
+                    return FindCardData(MergeData[i].ResultCard);
+                }
+            }
+        }
+        return null;
+    }
+}
