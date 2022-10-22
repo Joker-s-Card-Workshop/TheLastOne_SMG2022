c3a2e8caa15bc2ddae6aa1c0289b3b214017b36a
Black_Penguin <dkstmdwnswns@gmail.com>
2022-10-23 04:23:57 +0900

    StatusManager Save Fix

--- TheLastOne/Assets/02. Scripts/Manager/StatusManager.cs
+++ TheLastOne/Assets/02. Scripts/Manager/StatusManager.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
+[System.Serializable]
+public class SaveClass
+{
+    public bool[] isStageClear = new bool[6];
+}
 
 public class StatusManager : Singleton<StatusManager>
 {
     public bool[] isStageClear = new bool[6];
+    private SaveClass saveClass;
     private string savePath = "DataSavePath";
+    [SerializeField]
+    private bool DEBUG;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -14,13 +25,15 @@ public class StatusManager : Singleton<StatusManager>
     private void Start()
     {
         string saveData = PlayerPrefs.GetString(savePath, "null");
-        if (saveData != "null")
+        if (saveData != "null" && !DEBUG)
         {
-            isStageClear = JsonUtility.FromJson<bool[]>(saveData);
+            saveClass = JsonUtility.FromJson<SaveClass>(saveData);
+            isStageClear = saveClass.isStageClear;
         }
     }
     private void OnApplicationQuit()
     {
-        PlayerPrefs.SetString(savePath, JsonUtility.ToJson(isStageClear));
+        saveClass.isStageClear = isStageClear;
+        PlayerPrefs.SetString(savePath, JsonUtility.ToJson(saveClass));
     }
 }
