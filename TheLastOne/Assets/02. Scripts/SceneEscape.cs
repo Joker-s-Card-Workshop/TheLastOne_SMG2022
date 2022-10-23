d0bc9b93ae7987dabab97d80ea269dadee4027fe
Black_Penguin <dkstmdwnswns@gmail.com>
2022-10-23 10:29:51 +0900

    introScene

---
+++ TheLastOne/Assets/02. Scripts/SceneEscape.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneEscape : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
